4d2659459d029ba8e6ca59614f53511e5d8b5c9f
Van Kichline <vkichline@hotmail.com>
2020-09-28 20:08:01 -0700

    formatting

--- Apps/SoundMon/src/main.cpp
+++ Apps/SoundMon/src/main.cpp
@@ -2,39 +2,39 @@
 #include <driver/i2s.h>
 #include <fft.h>
 
-#define MODE_MIC                0
-#define MODE_SPK                1
-#define CONFIG_I2S_BCK_PIN      12
-#define CONFIG_I2S_LRCK_PIN     0
-#define CONFIG_I2S_DATA_PIN     2
-#define CONFIG_I2S_DATA_IN_PIN  34
-#define SPEAKER_I2S_NUMBER      I2S_NUM_0
+#define MODE_MIC                        0
+#define MODE_SPK                        1
+#define CONFIG_I2S_BCK_PIN              12
+#define CONFIG_I2S_LRCK_PIN             0
+#define CONFIG_I2S_DATA_PIN             2
+#define CONFIG_I2S_DATA_IN_PIN          34
+#define SPEAKER_I2S_NUMBER              I2S_NUM_0
 
 typedef struct {
-    uint8_t   state;
-    void*     audioPtr;
-    uint32_t  audioSize;
+    uint8_t           state;
+    void*             audioPtr;
+    uint32_t          audioSize;
 } i2sQueueMsg_t;
 
 
-static QueueHandle_t  fftvalueQueue = nullptr;
-static QueueHandle_t  i2sstateQueue = nullptr;
-TFT_eSprite           DisFFTbuff    = TFT_eSprite(&M5.Lcd);
+static QueueHandle_t  fftvalueQueue     = nullptr;
+static QueueHandle_t  i2sstateQueue     = nullptr;
+TFT_eSprite           DisFFTbuff        = TFT_eSprite(&M5.Lcd);
 
 
 bool InitI2SSpeakerOrMic(int mode) {
     esp_err_t err = ESP_OK;
 
     i2s_driver_uninstall(SPEAKER_I2S_NUMBER);
-    i2s_config_t i2s_config     = {
-        .mode                   = (i2s_mode_t)(I2S_MODE_MASTER),
-        .sample_rate            = 44100,
-        .bits_per_sample        = I2S_BITS_PER_SAMPLE_16BIT, // is fixed at 12bit, stereo, MSB
-        .channel_format         = I2S_CHANNEL_FMT_ONLY_RIGHT,
-        .communication_format   = I2S_COMM_FORMAT_I2S,
-        .intr_alloc_flags       = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count          = 2,
-        .dma_buf_len            = 128,
+    i2s_config_t i2s_config             = {
+        .mode                           = (i2s_mode_t)(I2S_MODE_MASTER),
+        .sample_rate                    = 44100,
+        .bits_per_sample                = I2S_BITS_PER_SAMPLE_16BIT, // is fixed at 12bit, stereo, MSB
+        .channel_format                 = I2S_CHANNEL_FMT_ONLY_RIGHT,
+        .communication_format           = I2S_COMM_FORMAT_I2S,
+        .intr_alloc_flags               = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count                  = 2,
+        .dma_buf_len                    = 128,
     };
     if (mode == MODE_MIC) {
         i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM);
@@ -48,10 +48,10 @@ bool InitI2SSpeakerOrMic(int mode) {
     err += i2s_driver_install(SPEAKER_I2S_NUMBER, &i2s_config, 0, NULL);
     i2s_pin_config_t tx_pin_config;
 
-    tx_pin_config.bck_io_num    = CONFIG_I2S_BCK_PIN;
-    tx_pin_config.ws_io_num     = CONFIG_I2S_LRCK_PIN;
-    tx_pin_config.data_out_num  = CONFIG_I2S_DATA_PIN;
-    tx_pin_config.data_in_num   = CONFIG_I2S_DATA_IN_PIN;
+    tx_pin_config.bck_io_num            = CONFIG_I2S_BCK_PIN;
+    tx_pin_config.ws_io_num             = CONFIG_I2S_LRCK_PIN;
+    tx_pin_config.data_out_num          = CONFIG_I2S_DATA_PIN;
+    tx_pin_config.data_in_num           = CONFIG_I2S_DATA_IN_PIN;
 
     err += i2s_set_pin(SPEAKER_I2S_NUMBER, &tx_pin_config);
     err += i2s_set_clk(SPEAKER_I2S_NUMBER, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
@@ -62,7 +62,7 @@ bool InitI2SSpeakerOrMic(int mode) {
 static void i2sMicroFFTtask(void *arg) {
     uint8_t   FFTDataBuff[128];
     uint8_t   FFTValueBuff[24];
-    uint8_t*  microRawData = (uint8_t*)calloc(2048,sizeof(uint8_t));
+    uint8_t*  microRawData              = (uint8_t*)calloc(2048,sizeof(uint8_t));
     size_t    bytesread;
     int16_t*  buffptr;
     double    data = 0;
@@ -125,7 +125,6 @@ static void i2sMicroFFTtask(void *arg) {
         }
     }
 }
-
 
 
 void MicroPhoneFFT() {

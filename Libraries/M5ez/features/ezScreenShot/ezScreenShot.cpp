176da8000a9d824083e271f084e53519c3ce8f89
Van Kichline <vkichline@hotmail.com>
2020-10-20 17:34:00 -0700

    improved screenshot

--- Libraries/M5ez/features/ezScreenShot/ezScreenShot.cpp
+++ Libraries/M5ez/features/ezScreenShot/ezScreenShot.cpp
@@ -3,10 +3,14 @@
 #include "ezScreenShot.h"
 
 
-#define			SNAP_DELAY					10000
+#define			SNAP_DELAY		10000
+#define			ACCEL_DELAY		500
 
 bool			ezScreenShot::_on;
 unsigned long	ezScreenShot::_last_snap;
+unsigned long	ezScreenShot::_last_accel;
+M5_Orientation	ezScreenShot::_last_orentation;
+M5_Orientation	ezScreenShot::_trigger_orentation;
 
 
 bool ezScreenShot::entry(uint8_t command, void* data) {
@@ -24,7 +28,8 @@ bool ezScreenShot::entry(uint8_t command, void* data) {
 
 
 void ezScreenShot::begin() {
-	_last_snap = 0;
+	_last_orentation = unknown_up;
+	_last_snap 		 = 0;
 	_read_flash();
 	ez.settings.menuObj.addItem("ScreenShot settings", ezScreenShot::menu);
 	ez.addEvent(ezScreenShot::loop);
@@ -37,16 +42,39 @@ void ezScreenShot::menu() {
 	menu.txtSmall();
 	menu.buttons("up#Back#select##down#");
 	menu.addItem("on|Enable ScreenShot\t" + (String)(_on ? "on" : "off"));
-		switch (menu.runOnce()) {
+	menu.addItem("trigger|Trigger\t" + get_orientation_name(_trigger_orentation) + " up", _select_trigger_menu);
+	while(int selection = menu.runOnce()) {
+		switch(selection) {
 			case 1:
 				_on = !_on;
+				menu.setCaption(1, "Enable ScreenShot\t" + (String)(_on ? "on" : "off"));
 				break;
-			case 0:
-				if (_on != start_state) {
-					_write_flash();
-				}
-				return;
+			case 2:
+				menu.setCaption(2, "Trigger\t" + get_orientation_name(_trigger_orentation) + " up");
+				break;
 		}
+	}
+	if (_on != start_state) {
+		_write_flash();
+	}
+}
+
+
+void ezScreenShot::_select_trigger_menu() {
+	ezMenu menu("ScreenShot Trigger");
+	menu.txtSmall();
+	menu.buttons("up#Back#select##down#");
+	menu.addItem("2|Back Up");
+	menu.addItem("4|Top Up");
+	menu.addItem("6|Bottom Up");
+	menu.addItem("3|Left Up");
+	menu.addItem("5|Right Up");
+	menu.addItem("1|Face Up");
+	if(menu.runOnce()) {
+		M5_Orientation old_orientation = _trigger_orentation;
+		_trigger_orentation = (M5_Orientation)atoi(menu.pickName().c_str());
+		if(_trigger_orentation != old_orientation) _write_flash();
+	}
 }
 
 
@@ -108,16 +136,16 @@ void ezScreenShot::snap() {
 	}
 	file.close();
 	free(data);
+	_last_snap = millis();
 }
 
 
 uint16_t ezScreenShot::loop() {
 	float	dummy, z;
-	if(_last_snap + SNAP_DELAY < millis()) {
-		M5.IMU.getAccelData(&dummy, &dummy, &z);
-		if(z < -0.9) {
+	if(_on && _last_snap + SNAP_DELAY < millis()) {
+		if(_trigger_orentation == get_orientation()) {
+			Serial.println("saving screenshot");
 			snap();
-			_last_snap = millis();
 		}
 	}
 	return _on ? 100 : 1000;
@@ -132,7 +160,8 @@ bool ezScreenShot::on() {
 void ezScreenShot::_read_flash() {
 	Preferences prefs;
 	prefs.begin("M5ez", true);	// read-only
-	_on = prefs.getBool("scrshot_on", false);
+	_on 				= prefs.getBool("scrshot_on", false);
+	_trigger_orentation = (M5_Orientation)prefs.getChar("scrshot_trigger", (char)back_up);
 	prefs.end();
 }
 
@@ -141,5 +170,36 @@ void ezScreenShot::_write_flash() {
 	Preferences prefs;
 	prefs.begin("M5ez", false);	// read-write
 	prefs.putBool("scrshot_on", _on);
+	prefs.putChar("scrshot_trigger", _trigger_orentation);
 	prefs.end();
+}
+
+
+M5_Orientation ezScreenShot::get_orientation() {
+  if (millis() - _last_accel < ACCEL_DELAY) return _last_orentation;
+  float 	  ax, ay, az;
+  const float threshold = 0.85;
+  _last_accel 			= millis();
+  _last_orentation 		= unknown_up;
+  M5.IMU.getAccelData(&ax, &ay, &az);
+  if      (az >  threshold) _last_orentation = face_up;
+  else if (az < -threshold) _last_orentation = back_up;
+  else if (ay >  threshold) _last_orentation = top_up;
+  else if (ay < -threshold) _last_orentation = bottom_up;
+  else if (ax >  threshold) _last_orentation = right_up;
+  else if (ax < -threshold) _last_orentation = left_up;
+  return _last_orentation;
+}
+
+
+String ezScreenShot::get_orientation_name(M5_Orientation orientation) {
+	switch(orientation) {
+		case unknown_up:	return "unknown";
+		case face_up:		return "face";
+		case back_up:		return "back";
+		case left_up:		return "left";
+		case top_up:		return "top";
+		case right_up:		return "right";
+		case bottom_up:		return "bottom";
+	}
 }
--- Libraries/M5ez/features/ezScreenShot/ezScreenShot.h
+++ Libraries/M5ez/features/ezScreenShot/ezScreenShot.h
@@ -1,5 +1,11 @@
 #pragma once
 
+// Orientation describes which way is up wrt the M5.
+// Normally, sitting on its botton, face_up would be the orientation.
+// Top and bottom refer to the top and bottom sides.
+//
+enum M5_Orientation { unknown_up, face_up, back_up, left_up, top_up, right_up, bottom_up };
+
 class ezScreenShot {
 	public:
 		static bool     		entry(uint8_t command, void* data);
@@ -8,9 +14,15 @@ class ezScreenShot {
 		static void     		snap();
 		static uint16_t 		loop();
 		static bool     		on();
+		static M5_Orientation	get_orientation();
+		static String           get_orientation_name(M5_Orientation orientation);
 	private:
 		static bool     		_on;
 		static unsigned long	_last_snap;
+		static unsigned long    _last_accel;
+		static M5_Orientation	_last_orentation;
+		static M5_Orientation	_trigger_orentation;
 		static void     		_read_flash();
 		static void     		_write_flash();
+		static void             _select_trigger_menu();
 };
